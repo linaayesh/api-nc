a4262113ebb45906a24b2c0b3f2a2373d493768c
Ibarhim-Jarada <ibrahim.jarada@gmail.com>
2022-07-03 18:14:39 +0300

    tsconfig types

--- server/src/__test__/index.test.ts
+++ server/src/__test__/index.test.ts
@@ -3,7 +3,7 @@ import supertest from 'supertest';
 import app from '../app';
 
 describe('/', () => {
-  it('should return 200 OK and Content-Type json', async () => {
+  test('should return 200 OK and Content-Type json', async () => {
     const res = await supertest(app)
       .get('/')
       .expect('Content-Type', /json/)
