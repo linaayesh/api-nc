547e14468dfffd4ef529c53869a8be1563dffd85
Rand <randsohail98@gmail.com>
2022-07-18 13:30:46 +0300

    edit response message in sign up

--- src/controllers/auth/signup.ts
+++ src/controllers/auth/signup.ts
@@ -16,9 +16,9 @@ export default async ({ body }: Request, res: Response, next: NextFunction):Prom
   try {
     await signupSchema.validateAsync(body);
     const userExists = await Users.findOne({ where: { email } });
-    if (userExists?.isApproved) throw new CustomError('This email is already used, and it is approved', 409);
-    if (userExists?.isRejected) throw new CustomError('This email is already used, and it is rejected', 409);
-    if (userExists) throw new CustomError('This email is already used, waiting for approval', 409);
+    if (userExists?.isApproved) throw new CustomError('This account is already in use', 409);
+    if (userExists?.isRejected) throw new CustomError('This account is rejected', 409);
+    if (userExists) throw new CustomError('This account is waiting for the admin approval', 409);
     const hashedPassword = await hash(password, 10);
     const user = await Users.create({
       username,
