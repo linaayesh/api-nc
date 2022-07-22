83d9df06adf706e351fe92ab8dfee79dd63b4373
Jood80 <shawar.nujood@gmail.com>
2022-07-22 19:28:37 +0300

    chore: update `users` http methods

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -10,9 +10,9 @@ const router = Router();
 // Middleware to check Admin
 router.use(isAdmin);
 router.get('/notApprovedUsers', notApprovedUsers);
-router.get('/approveUser/:userId', approveUser);
+router.post('/approveUser/:userId', approveUser);
 router.get('/approvedUsers', approvedUser);
-router.get('/rejectUser/:userId', rejectUser);
+router.post('/rejectUser/:userId', rejectUser);
 router.get('/rejectedUsers', rejectedUsers);
 
 export default router;
