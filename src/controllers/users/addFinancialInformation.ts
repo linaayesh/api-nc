0a63af03001d3fca75e705ae48e2f67c50bfb647
Rand <randsohail98@gmail.com>
2022-07-31 16:44:13 +0300

    feat: create add financial payments end point

---
+++ src/controllers/users/addFinancialInformation.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Response } from 'express';
+import { UserAuth } from '../../interfaces';
+import { CustomError, financialInformation } from '../../utilities';
+import { messages } from '../../helpers/constants';
+import { Payments } from '../../database/models';
+
+export default async (req: UserAuth, res: Response, next: NextFunction):Promise<void> => {
+  const { body } = req;
+  const { user } = req;
+  try {
+    const validationData = await financialInformation.validateAsync(body);
+
+    if (!user) throw new CustomError(messages.authResponse.notExist, 401);
+
+    const { name, address } = validationData;
+    const { id } = user;
+
+    await Payments.create({
+      name,
+      address,
+      createdBy: id,
+      userId: id,
+    });
+
+    res.status(201).json({ message: messages.authResponse.SUCCESS });
+  } catch (error) {
+    if (error instanceof Error) {
+      next(new CustomError(error.message, 400));
+    }
+  }
+};
