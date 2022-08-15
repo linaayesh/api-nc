84373a54a11fd6a8d40467f51697a94a67e562f8
Rand <randsohail98@gmail.com>
2022-08-15 12:40:41 +0300

    resolve conflict

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -25,11 +25,7 @@ router.get('/reset-password/:token', resetPasswordEmail);
 router.post('/forget-password', validator.body(emailSchema), forgetPassword);
 router.post('/reset-password', validator.body(passwordSchema), resetPassword);
 
-<<<<<<< HEAD
 router.use(checkUserRole([ADMIN, MASTER_ADMIN, COMEDIAN]));
-=======
-router.use(checkUserRole([SYSTEM, COMEDIAN]));
->>>>>>> 8023bce83c01023f5b5b6e97afb000dce02e475c
 
 router.get('/user', userAuth);
 router.patch('/edit-profile', validator.body(editProfileSchema), editProfile);
