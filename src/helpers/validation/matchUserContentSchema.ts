f1e1c805422b17b94df37164129b876ea3d77d94
zaher-aa <zaherabuamro@gmail.com>
2022-08-16 16:21:24 +0300

    Resolved review comments

--- src/helpers/validation/matchUserContentSchema.ts
+++ src/helpers/validation/matchUserContentSchema.ts
@@ -1,10 +1,10 @@
 import Joi from 'joi';
 
 export default Joi.object({
-  id: Joi.string().required(),
+  id: Joi.string().guid().required(),
   userId: Joi.number().min(1).required(),
   filmingCosts: Joi.number().min(0).required(),
   launchDate: Joi.string().isoDate().required(),
-  advance: Joi.number().required(),
-  feePaid: Joi.number().required(),
+  advance: Joi.number().min(0).required(),
+  feePaid: Joi.number().min(0).required(),
 });
