1ad376fafe3d0def85a10b834c0c87f627850fcf
zaher-aa <zaherabuamro@gmail.com>
2022-08-27 09:59:17 +0300

    fix: review comments (#33)

--- src/helpers/validation/changePasswordSchema.ts
+++ src/helpers/validation/changePasswordSchema.ts
@@ -2,6 +2,6 @@ import Joi from 'joi';
 
 export default Joi.object({
   oldPassword: Joi.string().required(),
-  password: Joi.string().required(),
+  password: Joi.string().invalid(Joi.ref('oldPassword')).messages({ 'any.invalid': "New password shouldn't be the same as the old one" }).required(),
   confirm: Joi.ref('password'),
 });
--- src/helpers/validation/getPaginatedDataSchema.ts
+++ src/helpers/validation/getPaginatedDataSchema.ts
@@ -4,5 +4,5 @@ export default Joi.object({
   limit: Joi.number().min(1).max(1000),
   page: Joi.number().min(1),
   title: Joi.string(),
-  id: Joi.string(),
+  id: Joi.string().guid(),
 });
