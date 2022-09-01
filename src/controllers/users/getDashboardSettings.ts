22bd01a5e328587793b89c43aa91525607229742
AymanSami98 <afahgan@students.iugaza.edu.ps>
2022-09-01 17:46:46 +0300

    update get data

--- src/controllers/users/getDashboardSettings.ts
+++ src/controllers/users/getDashboardSettings.ts
@@ -4,8 +4,12 @@ import { getDashboardSettings } from '../../services';
 export default async (_req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const data = await getDashboardSettings();
+    const newData = { ...data.regularVariables, ...data.encryptedVariables };
+    console.log(newData);
 
-    res.json({ data });
+    res.json({
+      data: newData,
+    });
   } catch (error) {
     next(error);
   }
