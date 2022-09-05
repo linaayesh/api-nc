98be9a19bd6a5d54c9059719bf5628a0f41fec1e
Ibarhim-Jarada <ibrahim.jarada@gmail.com>
2022-09-05 11:56:18 +0300

    perform calculations on previous reports based on the newly updated values during matching

--- src/services/user/matchUserContent.ts
+++ src/services/user/matchUserContent.ts
@@ -1,4 +1,11 @@
-import { Content, IContent } from 'db-models-nc';
+import {
+  Content, IContent, ContentReport, Report, getDashboardSettings,
+} from 'db-models-nc';
+import {
+  min, max, differenceInDays, addYears,
+} from 'date-fns';
+import Big from 'big.js';
+
 import { CustomError } from '../../helpers';
 import { HttpStatus } from '../../helpers/constants';
 
@@ -12,6 +19,7 @@ type IMatchUserContent = (_: {
   recoveredCosts: string,
 }) => Promise<IContent>
 
+const settings = getDashboardSettings();
 const matchUserContent: IMatchUserContent = async ({
   id,
   userId,
@@ -21,9 +29,19 @@ const matchUserContent: IMatchUserContent = async ({
   feePaid,
   recoveredCosts,
 }) => {
-  const content = await Content.findOne({ where: { id } });
-  if (!content) throw new CustomError('No such a content', HttpStatus.NOT_FOUND);
+  const content = await Content.findOne({
+    where: { id },
+    include: {
+      model: ContentReport,
+      as: 'contentReports',
+      include: [{
+        model: Report,
+      }],
+    },
+  });
 
+  if (!content) throw new CustomError('No such a content', HttpStatus.NOT_FOUND);
+  if (content.userId) throw new CustomError('Content is already matched with a user', HttpStatus.BAD_REQUEST);
   content.userId = userId;
   content.filmingCosts = filmingCosts;
   content.launchDate = launchDate;
@@ -31,6 +49,88 @@ const matchUserContent: IMatchUserContent = async ({
   content.feePaid = feePaid;
   content.recoveredCosts = recoveredCosts;
 
+  const { expiredAfterInYears, nextUpToOwedSplitPercentage } = await settings;
+
+  const owedSplitPercentage = new Big(1).minus(nextUpToOwedSplitPercentage);
+  const cReports = content.contentReports.map(({
+    id,
+    contentId,
+    reportId,
+    watchedSeconds,
+    revenue,
+    createdBy,
+    updatedBy,
+    tvodTicketsCount,
+    tvodSeconds,
+    watches,
+    report,
+  }) => {
+    if (!report) throw Error();
+    const toDate = new Date(report.watchTimeTo);
+    const fromDate:Date = new Date(report.watchTimeFrom);
+    const expDate = addYears(new Date(launchDate), expiredAfterInYears);
+    const markPointDate = max([min([expDate, toDate]), fromDate]);
+    const beforeExpiryReportDaysPercentage = new Big(1)
+      .times(differenceInDays(markPointDate, fromDate))
+      .div(differenceInDays(toDate, fromDate));
+    const beforeExpRevenue = beforeExpiryReportDaysPercentage.times(revenue);
+    const afterExpRevenue:Big = new Big(revenue).minus(beforeExpRevenue);
+    let nextupRevenue = afterExpRevenue.times(nextUpToOwedSplitPercentage);
+    let owedRevenue = afterExpRevenue.times(owedSplitPercentage);
+    const remainingCosts = new Big(filmingCosts).plus(feePaid).minus(recoveredCosts);// >=0
+    let splittableBeforeExpRevenue = beforeExpRevenue;
+    if (remainingCosts.gt(0)) {
+      splittableBeforeExpRevenue = splittableBeforeExpRevenue.minus(remainingCosts);
+      if (splittableBeforeExpRevenue.lt(0)) {
+        splittableBeforeExpRevenue = new Big(0);
+      }
+    }
+    const reimbursementBeforeExpRevenue = beforeExpRevenue
+      .minus(splittableBeforeExpRevenue);
+    nextupRevenue = nextupRevenue
+      .plus(splittableBeforeExpRevenue.times(nextUpToOwedSplitPercentage));
+    owedRevenue = owedRevenue
+      .plus(splittableBeforeExpRevenue.times(owedSplitPercentage));
+
+    content.nextUpAccRevenue = nextupRevenue
+      .plus(content.nextUpAccRevenue).toString();
+
+    content.owedAccRevenue = owedRevenue
+      .plus(content.owedAccRevenue).toString();
+    content.recoveredCosts = reimbursementBeforeExpRevenue
+      .plus(recoveredCosts).toString();
+
+    return ({
+      id,
+      contentId,
+      reportId,
+      watchedSeconds,
+      revenue,
+      createdBy,
+      updatedBy,
+      tvodTicketsCount,
+      tvodSeconds,
+      watches,
+      nextupRevenue: nextupRevenue.toString(),
+      owedRevenue: owedRevenue.toString(),
+      beforeExpiryReportDaysPercentage: beforeExpiryReportDaysPercentage.toString(),
+      beforeExpRevenue: beforeExpRevenue.toString(),
+      splittableBeforeExpRevenue: splittableBeforeExpRevenue.toString(),
+      reimbursementBeforeExpRevenue: reimbursementBeforeExpRevenue.toString(),
+      afterExpRevenue: afterExpRevenue.toString(),
+    });
+  });
+  await ContentReport.bulkCreate(cReports, {
+    updateOnDuplicate: [
+      'nextupRevenue',
+      'owedRevenue',
+      'beforeExpiryReportDaysPercentage',
+      'beforeExpRevenue',
+      'splittableBeforeExpRevenue',
+      'reimbursementBeforeExpRevenue',
+      'afterExpRevenue',
+    ],
+  });
   await content.save();
   return content;
 };
