3211a3a124daae42121b12e9f02160d7895c3be3
AymanSami98 <afahgan@students.iugaza.edu.ps>
2022-09-06 18:18:34 +0300

    create the function

--- src/services/user/getUserData.ts
+++ src/services/user/getUserData.ts
@@ -3,6 +3,8 @@ import { IUser, User } from 'db-models-nc';
 const getUserData = async (userId:number): Promise<IUser | null > => {
   const user = await User.findOne({
     where: { id: userId },
+    attributes: ['id', 'email', 'name'],
+
   });
   return user;
 };
